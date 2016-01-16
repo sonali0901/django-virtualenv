44840b47b7662cd167ffc94c889fd4ec5052d53c
sonali0901 <sonaligpt0@gmail.com>
2016-01-16 23:58:44 +0530

    17 jan

--- lnmiit_chat/chat/urls.py
+++ lnmiit_chat/chat/urls.py
@@ -4,7 +4,7 @@ from chat import views
  
 urlpatterns = patterns('', 
 	url(r'^$', views.index, name='index'),
-        url(r'^chat_room/', views.chat_room, name='chat_room'),
+        url(r'^chat_room/(\d+)/$', views.chat_room, name='chat_room'),
        
 )
 
